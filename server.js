200f37fd3fb7337f3d2aeea6572c40caa4543e44
Chris Figgins <figginsc@gmail.com>
2018-12-13 19:46:48 -0500

    Updated to add a count function and a port change

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var counter = 0;
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -44,20 +45,27 @@ var waitlist = [
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
+  counter++;
 });
 
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
+  counter++;
 });
 
 app.get("/table", function(req, res) {
   res.sendFile(path.join(__dirname, "table.html"));
+  counter++;
 });
 
 // Displays all characters
 app.get("/api/tables", function(req, res) {
   return res.json(tables);
 });
+
+app.get("/api/count", function(req, res) {
+  return res.json(counter);
+})
 
 // Create New Reservations - takes in JSON input
 app.post("/api/tables", function(req, res) {
